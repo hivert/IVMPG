878f71f15cb567c3d4a7dd179de27bb55ca44fa6
Florent Hivert <Florent.Hivert@u-psud.fr>
2014-04-26 18:16:16 +0200

    Added missing definition of Perm16::N

--- perm16.cpp
+++ perm16.cpp
@@ -1,6 +1,9 @@
 #include "perm16.hpp"
 #include <iostream>
 
+// Definition since previously *only* declared
+const constexpr size_t Vect16::N;
+
 std::ostream & operator<<(std::ostream & stream, Vect16 const &term) {
   stream << "[" << unsigned(term[0]);
   for (unsigned i=1; i < 16; i++) stream << "," << unsigned(term[i]);
